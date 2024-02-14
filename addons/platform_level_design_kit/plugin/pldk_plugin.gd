b488c9508f286a99f517d3e709286683d6f3d3da
Edmundo Ruiz Ghanem <contact@edmundito.com>
2024-02-14 15:21:03 -0500

    Add panel for platformer kit with readme

--- addons/platform_level_design_kit/plugin/pldk_plugin.gd
+++ addons/platform_level_design_kit/plugin/pldk_plugin.gd
@@ -1,15 +1,17 @@
 @tool
 extends EditorPlugin
 
+var _dock: Control
+
 func _init():
 	add_autoload_singleton("G", "res://addons/platform_level_design_kit/plugin/scripts/autoloads/globals.gd")
 	add_autoload_singleton("E", "res://addons/platform_level_design_kit/plugin/scripts/autoloads/engine.gd")
 
 func _enter_tree():
-	# Initialization of the plugin goes here.
-	pass
+	_dock = preload("res://addons/platform_level_design_kit/plugin/scenes/editor/dock_panel.tscn").instantiate()
+	add_control_to_dock(DOCK_SLOT_RIGHT_UL, _dock)
 
 
 func _exit_tree():
-	# Clean-up of the plugin goes here.
-	pass
+	remove_control_from_docks(_dock)
+	_dock.free()
