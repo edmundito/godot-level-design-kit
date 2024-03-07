567e56f2357b9647ed52f66172fc80d56f5dbe53
Edmundo Ruiz Ghanem <contact@edmundito.com>
2024-03-06 22:47:18 -0500

    Update exit to accept gems instead of coins

--- addons/platform_level_design_kit/plugin/scripts/engine/exit_door.gd
+++ addons/platform_level_design_kit/plugin/scripts/engine/exit_door.gd
@@ -1,7 +1,7 @@
 class_name ExitDoor
 extends Node3D
 
-@export var coins_required: int = 1
+@export var gems_required: int = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,8 +14,8 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	var completed: bool = E.coins >= coins_required
-	var message: String = "You did it!" if completed else "You need %d coins to go through the door!" % coins_required
+	var completed: bool = E.gems >= gems_required
+	var message: String = "You did it!" if completed else "You need %d gems to go through the door!" % gems_required
 	E.show_message.emit(message)
 	if completed:
 		E.completed_goal.emit()
