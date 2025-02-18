3fe95f2584e702a24dc8c5326a4b1b2c8b037cad
Edmundo Ruiz Ghanem <contact@edmundito.com>
2025-02-17 22:06:51 -0500

    Creat your first level scene Modify PatrolPath2Config export settings Add MovingPlatform2 using basic mesh object

--- engine/scripts/behaviors/patrol_path2_config.gd
+++ engine/scripts/behaviors/patrol_path2_config.gd
@@ -1,8 +1,7 @@
 class_name PatrolPath2Config extends Resource
 
 @export var speed := 1.0
-## The path that the Enemy will patrol.
-@export_custom(PROPERTY_HINT_ARRAY_TYPE, "PatrolPath2", PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_ALWAYS_DUPLICATE)
-var paths: Array[PatrolPath2] = []
+## The path that the object will use to patrol.
+@export var paths: Array[PatrolPath2] = []
 ## Enables path logging in Output panel.
 @export var log_path_changes := false
